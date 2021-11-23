0c332f72a559ca4c5c9f6a78026e003eb91892b9
Jorge Ercoli <73969725+jrercoli@users.noreply.github.com>
2021-11-23 20:22:28 -0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URI = os.getenv('DB_URI')
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ieqvxgrj:lAieKoszX_G7Z7a4IqTJecPL30LJv2eM@castor.db.elephantsql.com/ieqvxgrj"
+app.config["SQLALCHEMY_DATABASE_URI"] = 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
